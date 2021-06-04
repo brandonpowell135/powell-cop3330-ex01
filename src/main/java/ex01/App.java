80b5f8e3ae33100493d0dc85f0dec5f09cd75ab3
Brandon Desktop <brandonpowel35@gmail.com>
2021-06-03 20:18:44 -0400

    initial solution to problem 1

--- src/main/java/ex01/App.java
+++ src/main/java/ex01/App.java
@@ -1,4 +1,6 @@
 package ex01;
+
+import java.util.Scanner;
 /*
 Exercise 1 - Saying Hell
 Create a program that prompts for your name and prints a greeting using your name.
@@ -20,5 +22,14 @@ Write a version of the program that displays the greeting in a GUI window.
  */
 
 public class App {
-    
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        System.out.print("What is your name? ");
+        String name = in.nextLine();
+        //concat
+        String outputString = "Hello, " +name + ", nice to meet you!";
+
+        //output
+        System.out.println(outputString);
+    }
 }
